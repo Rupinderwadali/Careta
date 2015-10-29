de835429c7161be257f3056e14c3014fed18ec46
Rupinderwadali <rupinderwadali@gmail.com>
2015-10-29 18:39:25 +0530

    TEmplate

---
+++ content-arealist.php
@@ -0,0 +1,39 @@
+<div class="area">
+<?php
+$terms = get_terms('area');
+foreach($terms as $term) : ?>
+        <div class = "<?php echo $term->name ?>">
+                <?php
+                $posts = new WP_Query(array('area' => $term->slug));
+
+                if( $posts->have_posts() ):?>
+                <div class="room"></div>
+                <?php   while( $posts->have_posts() ) :
+                       $posts->the_post();?>
+                         <?php the_post_thumbnail();?>
+
+        <div class="post_title">
+                <a href ="<?php the_permalink();?>"><?php the_title();?></a>
+                </div>
+        <div class="btn1">
+                <input type="button" id="btnclick" value="Order" />
+        </div>
+                <?php
+                        endwhile;
+                endif;
+                ?>
+        </div><!-- Chheharta -->
+<?php
+endforeach; ?>
+
+<?php //wp_reset_postdata();
+?>
+</div>
+<script type="text/javascript">
+$(function() {
+$("#btnclick").click(function() {
+var url = 'http://localhost/wordpress/index.php/place-order/';
+$(location).attr('href', url);
+})
+                                                                                                                             1,1           Top
+
